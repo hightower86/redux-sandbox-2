864f107b0db60df602f51a0570204febbd553b5a
Adel <htwr86@gmail.com>
2019-09-04 23:40:54 +0300

    styling counter component

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -4,8 +4,13 @@ import './Counter.scss';
 
 const Counter = () => {
   return (
-    <div>
-      <h2>Counter</h2>
+    <div className='counter'>
+      <h2>0</h2>
+      <div className='buttons'>
+        <button>INC</button>
+        <button>DEC</button>
+        <button>RND</button>
+      </div>
     </div>
   );
 };
