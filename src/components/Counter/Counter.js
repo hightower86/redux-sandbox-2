3aa844b6d53be25ea74999bfce661f21ffb89564
Adel <htwr86@gmail.com>
2019-09-05 08:50:31 +0300

    inc dec actions and mapDispatchToProps

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,18 +1,37 @@
 import React from 'react';
-
+import { connect } from 'react-redux';
 import './Counter.scss';
 
-const Counter = () => {
+const Counter = ({ counter, inc, dec }) => {
   return (
     <div className='counter'>
-      <h2>0</h2>
+      <h2>counter: {counter}</h2>
       <div className='buttons'>
-        <button>INC</button>
-        <button>DEC</button>
+        <button onClick={inc}>INC</button>
+        <button onClick={dec}>DEC</button>
         <button>RND</button>
       </div>
     </div>
   );
 };
 
-export default Counter;
+const actions = {
+  inc: { type: 'INC' },
+  dec: { type: 'DEC' }
+};
+
+const mapStateToProps = state => {
+  return { counter: state.counter };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    inc: () => dispatch(actions.inc),
+    dec: () => dispatch(actions.dec)
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 import './index.scss';
 import App from './components/App/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {
+  counter: 10
+};
+
+const reducer = (state = initialState, action) => {
+  const newState = { ...state };
+  switch (action.type) {
+    case 'INC':
+      newState.counter += 1;
+      return newState;
+    case 'DEC':
+      newState.counter -= 1;
+      return newState;
+
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+
+  document.getElementById('root')
+);
