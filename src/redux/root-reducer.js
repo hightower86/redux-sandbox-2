2b4888429799b920346975d529f38af5e0ca54ef
Adel <htwr86@gmail.com>
2019-09-05 19:16:40 +0300

    initial commit - 3

--- src/redux/root-reducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from 'redux';
-
-import { counterReducer } from '../redux/counter/counter-reducer';
-
-export const rootReducer = combineReducers({
-  cR: counterReducer
-});
