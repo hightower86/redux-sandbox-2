85fbbcbe5fc4d3d677f7eac32ce179bcbd3a4b85
Adel <htwr86@gmail.com>
2019-09-09 19:27:30 +0300

    counter component finished redux-sandbox-6

--- src/components/Counter/Counter.js
+++ src/components/Counter/Counter.js
@@ -1,18 +1,39 @@
 import React from 'react';
 
+import { connect } from 'react-redux';
+
 import './Counter.scss';
 
-const Counter = () => {
+const Counter = ({ counter, inc, dec, rnd }) => {
   return (
     <div className='counter'>
-      <h3 className='title'>Counter Component</h3>
+      <h3 className='title'>{counter}</h3>
       <div className='buttons'>
-        <button className='button'>PLUS</button>
-        <button className='button'>MINUS</button>
-        <button className='button'>RANDOM</button>
+        <button className='button' onClick={inc}>
+          PLUS
+        </button>
+        <button className='button' onClick={dec}>
+          MINUS
+        </button>
+        <button className='button' onClick={rnd}>
+          RANDOM
+        </button>
       </div>
     </div>
   );
 };
 
-export default Counter;
+const mapStateToProps = state => ({
+  counter: state.counter
+});
+
+const mapDispatchToProps = dispatch => ({
+  inc: () => dispatch({ type: 'INC' }),
+  dec: () => dispatch({ type: 'DEC' }),
+  rnd: () => dispatch({ type: 'RND', payload: Math.floor(Math.random() * 10) })
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,30 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
 import App from './components/App';
 import './index.scss';
 
-ReactDom.render(<App />, document.getElementById('root'));
+const reducer = (state = { counter: 0 }, action) => {
+  switch (action.type) {
+    case 'INC':
+      return { ...state, counter: (state.counter += 1) };
+    case 'DEC':
+      return { ...state, counter: (state.counter -= 1) };
+    case 'RND':
+      return { ...state, counter: (state.counter += action.payload) };
+
+    default:
+      return state;
+  }
+};
+const store = createStore(reducer);
+
+ReactDom.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
