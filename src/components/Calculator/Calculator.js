031b5dcf8f8cd428ad5c7e7b4d783310af22dfb2
Adel <htwr86@gmail.com>
2019-09-16 15:40:50 +0300

    add digital font to display

--- src/components/Calculator/Calculator.js
+++ src/components/Calculator/Calculator.js
@@ -6,18 +6,21 @@ const Calculator = () => {
     <div className='calculator'>
       {/* <h2>Calculator</h2> */}
       <div className='box'>
-        <div className='display'></div>
+        <div className='display'>456789.0</div>
         <div className='keyboard'>
           <div className='digits'>
-            <button className='btn'></button>
-            <button className='btn'></button>
-            <button className='btn'></button>
-            <button className='btn'></button>
-            <button className='btn'></button>
-            <button className='btn'></button>
-            <button className='btn'></button>
-            <button className='btn'></button>
-            <button className='btn'></button>
+            <button className='btn'>1</button>
+            <button className='btn'>2</button>
+            <button className='btn'>3</button>
+            <button className='btn'>4</button>
+            <button className='btn'>5</button>
+            <button className='btn'>6</button>
+            <button className='btn'>7</button>
+            <button className='btn'>8</button>
+            <button className='btn'>9</button>
+            <button className='btn'>+-</button>
+            <button className='btn'>0</button>
+            <button className='btn'>,</button>
           </div>
           <div className='actions'>
             <button className='act'></button>
